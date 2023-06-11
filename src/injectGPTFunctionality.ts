84dce21e130c0b326522447d9cb3ad2c43642daf
bruno <bruno.resende@gmx.com>
2023-06-11 15:55:30 -0300

    Feature: GPT Send button also send Chunks

---
+++ src/injectGPTFunctionality.ts
@@ -0,0 +1,18 @@
+import { chatGPTElements, gptExtendElements } from './getPageElements';
+import { handleChunkInput } from './utils/splitAndSend';
+
+export function injectGptFunctionality() {
+    const { textArea, sendButton } = chatGPTElements();
+    const { gptXtendContainer } = gptExtendElements();
+
+    textArea?.addEventListener('keydown', (event: KeyboardEvent) => {
+        if (event.key === 'Enter' && event.altKey) {
+            handleChunkInput();
+        }
+    });
+
+    sendButton?.addEventListener('click', () => {
+        if (gptXtendContainer?.classList.contains('hidden')) return;
+        handleChunkInput();
+    });
+}
